d201f05b02ae56e3f5d6f07cb395983ba979f8d6
backenklee <lennart-duehrsen@gmx.net>
2013-08-05 16:25:15 +0200

    added task 21

---
+++ Uebung-1/src/aufgaben/Auf21.java
@@ -0,0 +1,14 @@
+package aufgaben;
+
+public class Auf21 {
+	
+	public static void main(String[] args) {
+		int c = 5;
+		int d = 5;
+		c = c++;
+		System.out.println(c);
+		d = ++d;
+		System.out.println(d);
+	}
+
+}
