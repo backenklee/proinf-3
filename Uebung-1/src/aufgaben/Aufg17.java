deaa154ed12c0cb2965e34fb09d84a8657524545
yogo1212 <leon@georgemail.de>
2013-08-05 14:10:46 +0200

    Aufgabe 21 - 17 halb

---
+++ Uebung-1/src/aufgaben/Aufg17.java
@@ -0,0 +1,9 @@
+package aufgaben;
+
+public class Aufg17 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
---
+++ Uebung-1/src/aufgaben/Aufg19.java
@@ -0,0 +1,16 @@
+package aufgaben;
+
+public class Aufg19 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 2; i++) {
+			SprungZuJ: for (int j = 0; j <= 2; j++) {
+				for (int k = 0; k <= 2; k++) {
+					if (k == 1)
+						break SprungZuJ;
+				}
+			}
+		}
+		System.out.println("Le shit terminiert nicht");
+	}
+}
