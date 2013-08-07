5fbba117005891d4fabe290179c059f08852c698
backenklee <lennart-duehrsen@gmx.net>
2013-08-07 12:45:54 +0200

    updated Auf02

--- Uebung-2/src/aufgaben/Auf01.java
+++
@@ -1,9 +0,0 @@
-package aufgaben;
-
-public class Auf01 {
-	
-	public static void main(String[] args) {
-		System.out.println("YAY");
-	}
-
-}
--- Uebung-2/src/aufgaben/Auf02.java
+++ Uebung-2/src/aufgaben/Auf02.java
@@ -1,25 +1,20 @@
 package aufgaben;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class Auf02 {
-
-	public static void main(String[] args) {
-		try {
-			BufferedWriter wrt = new BufferedWriter(new FileWriter("blablabla"));
-			for (byte zhlr = 0; zhlr < 100; zhlr++) {
-				for (byte zhlr2 = 0; zhlr2 < 10; zhlr2++) {
-					wrt.write((zhlr * zhlr + zhlr2)+"\t");
-				}
-			}
-			wrt.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
+    
+    public static void main(String[] args) throws IOException {
+        OutputStream os = new FileOutputStream(new File("auf02-output.txt"));
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 10; j++) {
+                int res = i*i+j;
+                osw.write(res + " ");
+            }
+            osw.write("\n");
+        }
+        osw.close();
+    }
 
 }
