e54f0945ab54070de83cb46264a3b73e114fa8d3
backenklee <lennart-duehrsen@gmx.net>
2013-08-07 13:12:26 +0200

    updated shit

--- Uebung-2/src/aufgaben/Auf01.java
+++ Uebung-2/src/aufgaben/Auf01.java
@@ -3,7 +3,7 @@ package aufgaben;
 public class Auf01 {
 	
 	public static void main(String[] args) {
-		System.out.println("Muehsam!");
+		System.out.println("Mühsam!");
 	}
 
 }
--- Uebung-2/src/aufgaben/Auf02.java
+++ Uebung-2/src/aufgaben/Auf02.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class Auf02 {
     
     public static void main(String[] args) throws IOException {
-        OutputStream os = new FileOutputStream(new File("auf02-output.txt"));
+        OutputStream os = new FileOutputStream(new File("auf02-out.txt"));
         OutputStreamWriter osw = new OutputStreamWriter(os);
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j < 10; j++) {
