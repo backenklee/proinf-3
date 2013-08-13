787bfd9c8ad724a6b584e82a9b6e38ee136b3dab
backenklee <lennart-duehrsen@gmx.net>
2013-08-13 12:27:55 +0200

    removed some sysos

--- Uebung-3/src/aufgaben/Auf08.java
+++ Uebung-3/src/aufgaben/Auf08.java
@@ -14,17 +14,11 @@ public class Auf08 {
 		double vz = 1;
 		double zaehler, nenner, res;
 		for (int i = 1; i <= it; i += 2) {
-			System.out.println("vz: " + vz);
-			System.out.println("i: " + i);
 			zaehler = Math.pow(x, i);
-			System.out.println("zaehler: " + zaehler);
 			nenner = fac(i);
-			System.out.println("nenner: " + nenner);
 			res = vz*(zaehler/nenner);
-			System.out.println("res: " + res);
 			sin += res;
 			vz *= -1.0;
-			System.out.println();
 		}
 		return sin;
 	}
